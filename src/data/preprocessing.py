59c5b9d4025341f3eff020b5bf241226ced04a40
Jose R. Zapata <JoseRZapata@users.noreply.github.com>
2023-06-05 06:56:21 -0500

    Feature/actualizar notebooks (#6)
    * fix remove patien duplicates keep first
    
    * update notebnook 2 and reports
    
    * update notebook 3
    
    * no medical reduction feature
    
    * updat4 notebook 4
    
    * actualizacion notebook 5

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -22,21 +22,25 @@ def process_data(data: pd.DataFrame) -> pd.DataFrame:
 # remove duplicates by patient_nbr in this step to avoid 
 # same person in train and test
 
+
 # function to print shape of the dataframe
 def print_shape(data: pd.DataFrame, msg: str = 'Shape =') -> pd.DataFrame:
     """Print shape of dataframe."""
     print(f'{data.shape}{msg}')
     return data
 
+
 def sort_data(data: pd.DataFrame, col: str) -> pd.DataFrame:
     "Sort data by and specific column"
     data = data.sort_values(by=col,ascending= False)
     return data
 
+
 # remove duplicates from data based on a column
 def drop_exact_duplicates(data: pd.DataFrame) -> pd.DataFrame:
     """Drop duplicate rows from data."""
-    return data.drop_duplicates(keep=False)
+    return data.drop_duplicates(keep='first')
+
 
 # remove duplicates from data based on a column
 def drop_duplicates(data: pd.DataFrame,
@@ -45,6 +49,7 @@ def drop_duplicates(data: pd.DataFrame,
     data = data.drop_duplicates(subset=drop_cols, keep='first')    
     return data
 
+
 def transform_output(data: pd.DataFrame) -> pd.DataFrame:
     """ Replace target column to 1 and 0 values"""
 
@@ -52,4 +57,5 @@ def transform_output(data: pd.DataFrame) -> pd.DataFrame:
                                 '>30':0,
                                 'NO':0},
                                 inplace=True)
-    return data                                
+    return data
+                                
